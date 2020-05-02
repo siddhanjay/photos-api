acf67f359b0a33073179870bb2c52f2d7adce9f0
siddhanjay <godre007@yahoo.com>
2020-05-03 02:44:01 +0800

    add admin panel

--- photos/urls.py
+++ photos/urls.py
@@ -1,4 +1,5 @@
 from photos import views
+from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -14,4 +15,5 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', registration, name='register'),
+    path('admin/', admin.site.urls),
 ]
